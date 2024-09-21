deea5acabb7101af568575ca95c7313bf7611794
kenny <kenny909@mat.umk.pl>
2024-09-21 15:03:25 +0200

    Create header

---
+++ src/app/header..tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { BsSearch, BsBell } from "react-icons/bs";
+
+export default function Header() {
+  return (
+    <header className="bg-neutral-800 px-4 py-5 flex gap-4 items-center">
+      <Link href='/search' className="flex grow">
+        <input
+          className="grow bg-zinc-900 p-3"
+          placeholder="Search for..."
+        />
+
+        <div className="grow-0 bg-emerald-950 p-3 grid content-center" >
+          <button>
+            <BsSearch size={24} />
+          </button>
+        </div>
+      </Link>
+
+      <button>
+        <BsBell
+          className="grow-0"
+          size={24}
+        />
+      </button>
+    </header>
+  );
+}
