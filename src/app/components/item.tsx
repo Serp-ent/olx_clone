6282df7c9a6f566c944b5afa14c13801307dd0b7
kenny <kenny909@mat.umk.pl>
2024-10-06 16:02:17 +0200

    Improve appearance of item

--- src/app/components/item.tsx
+++ src/app/components/item.tsx
@@ -17,7 +17,7 @@ export default function Item({ item }: { item: ProductWithImages }) {
       className="bg-white flex h-24 justify-between shadow border rounded-md p-2 gap-4"
     >
       <Link
-        className="flex items-center gap-2 grow"
+        className="flex gap-2 grow"
         href={`/offer/${item.id}`}
       >
         <div className="border-2 rounded-md aspect-square relative h-full">
@@ -29,18 +29,34 @@ export default function Item({ item }: { item: ProductWithImages }) {
           />
         </div>
 
+        {/* // TODO: add information if item was used below */}
+        <div className="flex flex-col grow truncate justify-between">
+          {/* // TODO: add hover that switches to bg:primary and text white */}
+          <h3 className="grow">
+            {item.name}
+          </h3>
 
-        <h3 className="grow">
-          {item.name}
-        </h3>
+          <h6 className="text-[0.6rem] text-gray-500">
+            TODO: localization
+          </h6>
+
+        </div>
+
       </Link>
 
-      <div className="text-2xl grid items-center pr-2">
-        <FavoriteButton
-          itemId={item.id.toString()}
-          // TODO: fix hardcoded value to receive it from db
-          initialIsFavorite={item.isFavorite}
-        />
+      <div className="flex flex-col justify-between">
+        <section className="font-semibold text-sm">
+          {item.price.toString()} zl
+          {/* // TODO: below price should be info if price is negotiable */}
+        </section>
+
+        <div className="pr-2 flex justify-end">
+          <FavoriteButton
+            itemId={item.id.toString()}
+            initialIsFavorite={item.isFavorite}
+          />
+        </div>
+
       </div>
     </div>
   );
